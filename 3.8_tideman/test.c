a25af108f26ef8aa19939e4b72c3d857693ea825
geanluiz <97045487+geanluiz@users.noreply.github.com>
2024-01-26 14:58:44 -0300

    update files3

--- 3.8_tideman/test.c
+++
@@ -1,8 +0,0 @@
-#include "../../97045487/3.8_tideman/tideman.c"
-#include <stdio.h>
-#include <stdlib.h>
-
-void test_candidates(void)
-{
-    system("../../97045487/3.8_tideman/tideman Alice Bob Charlie");
-}
