ce5700d8f64d05d6f886abb5accdeda896060943
geanluiz <97045487+geanluiz@users.noreply.github.com>
2024-01-26 14:41:13 -0300

    update files2

---
+++ 3.8_tideman/test.c
@@ -0,0 +1,8 @@
+#include "../../97045487/3.8_tideman/tideman.c"
+#include <stdio.h>
+#include <stdlib.h>
+
+void test_candidates(void)
+{
+    system("../../97045487/3.8_tideman/tideman Alice Bob Charlie");
+}
---
+++ 3.8_tideman/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Simulates user entry
+./tideman Alice Bob Charlie < ./input.txt > ./log.txt
