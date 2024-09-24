aaf1bdc9f1f8a4064b4ef69b6ee3eab50121b796
Gean Luiz S Ribeiro <geanluiz1@gmail.com>
2024-09-23 22:31:58 -0300

    update #2 at 23-09-2024

--- 7.3_fiftyville/log.sql
+++ 7.3_fiftyville/log.sql
@@ -23,18 +23,29 @@ SELECT name, hour, minute, activity, people.license_plate FROM people
 JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
 WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25;
 
--- Check calls log for the lead about the thief call after the theft that were less than a minute
-SELECT name, phone_number, duration FROM people
-JOIN phone_calls ON people.phone_number = phone_calls.caller
-WHERE year = 2023 AND month = 7 AND day = 28 AND duration < 60;
+SELECT '';
 
--- Check atm transactions that have occured near the theft
-SELECT DISTINCT name FROM people
+-- Check calls log for the lead about the thief call after the theft that were less than a minute
+SELECT * FROM
+(SELECT name, phone_number, duration FROM people
+JOIN phone_calls ON people.phone_number = phone_calls.caller
+WHERE year = 2023 AND month = 7 AND day = 28 AND duration < 60) c1
+LEFT JOIN
+(SELECT name, phone_number, duration FROM people
+JOIN phone_calls ON people.phone_number = phone_calls.receiver
+WHERE year = 2023 AND month = 7 AND day = 28 AND duration < 60) c2
+ON c1.name = c2.name;
+
+
+-- Searches for the atm transaction that the witness saw
+SELECT DISTINCT name, transaction_type FROM people
 JOIN bank_accounts ON person_id = people.id
 JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
 WHERE year = 2023 AND month = 7 AND day = 28
-AND atm_location LIKE '%Humphrey%';
+AND atm_location LIKE '%Leggett%' AND transaction_type = 'withdraw';
 
+
+SELECT '';
 
 -- The query below returns 4 names that exited the parking lot
 -- between 10h15-10h25 and also made a call roughly at the same period for less than a minute
@@ -49,6 +60,8 @@ AND duration < 60;
 
 SELECT * FROM conc;
 
+SELECT '';
+
 -- This one checks all of the above informations against each other to see if there is a match
 SELECT DISTINCT name, people.id FROM people
 JOIN bank_accounts ON person_id = people.id
@@ -56,8 +69,10 @@ JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account
 WHERE atm_transactions.year = 2023
 AND atm_transactions.month = 7
 AND atm_transactions.day = 28
-AND atm_location LIKE '%Humphrey%'
+AND atm_location LIKE '%Leggett%' AND transaction_type = 'withdraw'
 AND people.name IN (SELECT * FROM conc);
+
+SELECT '';
 
 /* Above query didn't returned any result, so that maybe confirms my suspection with Eugene's testimony? */
 
@@ -68,7 +83,7 @@ JOIN passengers ON people.passport_number = passengers.passport_number
 JOIN flights ON passengers.flight_id = flights.id
 JOIN airports ON flights.origin_airport_id = airports.id
 WHERE name IN (SELECT * FROM conc)
-AND flights.year = 2023 AND flights.month = 7 AND flights.day = 29 AND full_name LIKE 'Fiftyville%') t1
+AND flights.year = 2023 AND flights.month = 7 AND flights.day = 29) t1
 
 INNER JOIN
 
@@ -80,3 +95,8 @@ WHERE name IN (SELECT * FROM conc)
 AND flights.year = 2023 AND flights.month = 7 AND flights.day = 29) t2
 
 ON t1.name = t2.name;
+
+SELECT '';
+
+SELECT * FROM phone_calls
+WHERE year = 2023 AND month = 7 AND day = 28 AND duration < 60;
