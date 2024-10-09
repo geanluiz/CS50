71507485c399fc225a3901a36871444f5740e4c6
geanluiz <97045487+geanluiz@users.noreply.github.com>
2024-10-09 15:48:51 -0300

    update at 09-10-2024

--- 8.2_homepage/script.js
+++ 8.2_homepage/script.js
@@ -1,23 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
-    localStorage.setItem('mode', 'dark');
-    let mode = localStorage.getItem('mode');
 
-    let icon = document.querySelector('#theme-img');
-    let body = document.querySelector('body');
+
+    /* video: https://www.youtube.com/watch?v=2j_kBqpFK-g */
+
+
+    if (localStorage.getItem('mode') === null){
+        localStorage.setItem('mode', 'dark');
+    }
+    var mode = localStorage.getItem('mode');
+    const body = document.querySelector('body');
 
     body.setAttribute('data-bs-theme', mode);
 
+    const icon = document.querySelector('#theme-img');
+
+    if (mode === 'light'){
+        icon.src = 'dark-mode.png';
+        body.setAttribute('data-bs-theme', 'light');
+    } else if (mode === 'dark'){
+        icon.src = 'light-mode.png';
+        body.setAttribute('data-bs-theme', 'dark');
+    };
+
     icon.addEventListener('click', function() {
-        if (mode === 'dark'){
-            localStorage.setItem('mode', 'light');
+        console.log('click');
+        console.log(mode);
 
-            body.setAttribute('data-bs-theme', mode);
-            icon.setAttribute('src', 'light-mode.png'); // Black-icon
-        } else {
+        if (body.getAttribute('data-bs-theme') == 'light'){
+            icon.src = 'light-mode.png';
+            localStorage.setItem('mode', 'dark');
+            body.setAttribute('data-bs-theme', 'dark');
+            console.log(mode);
+        } else if (body.getAttribute('data-bs-theme') === 'dark'){
+            icon.src = 'dark-mode.png';
             localStorage.setItem('mode', 'light');
-
-            body.setAttribute('data-bs-theme', mode);
-            icon.setAttribute('src', 'dark-mode.png'); // Clear-icon
-        }
+            body.setAttribute('data-bs-theme', 'light');
+            console.log(mode);
+        };
     });
 });
